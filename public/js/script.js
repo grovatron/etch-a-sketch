17b8f1a2d2daf725787334df9d9d6a126639d268
grovatron <grover.a.sundstrom@gmail.com>
2018-06-01 18:59:48 -0400

    Implement random color and pixel shading functions

--- public/js/script.js
+++ public/js/script.js
@@ -23,7 +23,7 @@ function createPixels(number) {
     for (let i = 0; i < Math.pow(number, 2); i++) {
         let pixel = document.createElement('div');
         pixel.style.border = '1px gray solid';
-        pixel.addEventListener('mouseover', makeGray);
+        pixel.addEventListener('mouseover', shadePixel);
         pixels.push(pixel);
     }
 }
@@ -39,8 +39,41 @@ function createGrid(number) {
     pixels.forEach(pixel=>container.appendChild(pixel));
 }
 
-function makeGray(event) {
-    this.style.backgroundColor = "#929795";
+function makeBlack(event) {
+    this.style.backgroundColor = "rgb(0,0,0)";
+}
+
+function shadePixel(event) {
+    if (this.style.backgroundColor === "") {
+        this.style.backgroundColor = 'rgba(0,0,0,.1)';
+        console.log('pixel shaded');
+        return;
+    }
+    increaseShade(this);
+}
+
+function increaseShade(pixel) {
+    if (!pixel.style.backgroundColor.includes('rgba')) {
+        return;
+    }
+    let bgColor = pixel.style.backgroundColor;
+    let currentShade = parseFloat(bgColor.substring(bgColor.length - 4, bgColor.length - 1));
+    pixel.style.backgroundColor = `rgba(0,0,0,${currentShade + .1})`;
+}
+
+function makeRandomColor(event) {
+    this.style.backgroundColor = getRandomColor();
+}
+
+function getRandomColor() {
+    let r = getRandomRGBValue();
+    let g = getRandomRGBValue();
+    let b = getRandomRGBValue();
+    return `rgb(${r},${g},${b})`;
+}
+
+function getRandomRGBValue() {
+    return Math.floor(Math.random() * 256);
 }
 
 function resetGrid() {
